dbc6b912c93461a409f5715a762f4d6db4189142
marzsam <marzsam@proton.me>
2023-02-06 20:47:42 +0100

    stack interface implemented

---
+++ org/java/stack/EmptyStackException.java
@@ -0,0 +1,7 @@
+package org.java.stack;
+
+public class EmptyStackException extends RuntimeException{
+    public EmptyStackException(String msg){
+        super(msg);
+    }
+}
---
+++ org/java/stack/Stack.java
@@ -0,0 +1,15 @@
+package org.java.stack;
+
+public interface Stack<E> {
+    
+    public void push(E e);
+
+    public E pop() throws EmptyStackException;
+
+    public int size();
+
+    public boolean isEmpty();
+
+    public E top() throws EmptyStackException;
+
+}
